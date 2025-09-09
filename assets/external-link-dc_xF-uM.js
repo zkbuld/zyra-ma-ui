3119039caccae0a2c16c0583e196dc0b29791f7d
bzkdev02 <bzkdev02@users.noreply.github.com>
2025-09-09 07:16:04 +0000

    deploy: 86ff1ebeed8e6af56726733485c1b1f084aa050d

--- assets/external-link-dc_xF-uM.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-CPjXpX9g.js";import"./index-DBaTYUcs.js";import"./index.es-CJfxZB_o.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const n=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{n as externalLinkSvg};
