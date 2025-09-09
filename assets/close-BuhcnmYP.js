7ecd6f94aeea5973fc08b12d83c7cb7f9d565dcd
bzkdev02 <bzkdev02@users.noreply.github.com>
2025-09-09 07:41:37 +0000

    deploy: 79138dc644b62e76c5182679b95a7e4e5de8c0dd

--- assets/close-BuhcnmYP.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-B5k2mWJU.js";import"./index-DxUlHS8y.js";import"./index.es-CgQ3_iyC.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M2.54 2.54a1 1 0 0 1 1.42 0L8 6.6l4.04-4.05a1 1 0 1 1 1.42 1.42L9.4 8l4.05 4.04a1 1 0 0 1-1.42 1.42L8 9.4l-4.04 4.05a1 1 0 0 1-1.42-1.42L6.6 8 2.54 3.96a1 1 0 0 1 0-1.42Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as closeSvg};
