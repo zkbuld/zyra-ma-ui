f7d0dc8aa698ba57b1e6508554dcae79c36f9ca7
bzkdev02 <bzkdev02@outlook.com>
2025-09-09 13:52:22 +0800

    [test] fix

--- eslint.config.js
+++ eslint.config.js
@@ -3,27 +3,19 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
+import { globalIgnores, defineConfig } from "eslint/config";
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "react-refresh/only-export-components": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-    },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+export default defineConfig(globalIgnores(["dist"]), {
+  files: ["**/*.{ts,tsx}"],
+  extends: [js.configs.recommended, tseslint.configs.recommended, reactHooks.configs["recommended-latest"], reactRefresh.configs.vite],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react-refresh/only-export-components": "off",
+    "@typescript-eslint/no-unused-expressions": "off",
   },
-]);
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+});
